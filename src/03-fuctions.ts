78f4270d501eb3fbfd8442091de00f31a59523a0
jhonperez95 <jhonperez.j95@gmail.com>
2022-05-12 23:14:26 -0500

    nombres de funciones factorizado

---
+++ src/03-fuctions.ts
@@ -0,0 +1,38 @@
+;(() => {
+  // función para obtener información de una película por Id
+  function getMovieById(id: string) {
+    console.log({ id })
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieCastByMovie(movieId: string) {
+    console.log({ movieId })
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getBioActorById(actorId: string) {
+    console.log({ actorId })
+  }
+
+  // Crear una película
+  interface Movie {
+    cast: string[]
+    description: string
+    rating: number
+    title: string
+  }
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast })
+  }
+
+  // Crea un nuevo actor
+  function createActor(fullName: string, birthdate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === 'fernando') return false
+
+    console.log('Crear actor')
+    return true
+  }
+})()
