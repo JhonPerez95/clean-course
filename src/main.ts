cc696f3352c4a99193c0b81faafa617f316adba3
jhonperez95 <jhonperez.j95@gmail.com>
2022-05-12 23:59:44 -0500

    clean

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import './style.css'
-
+import './04-homework'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
 `
-
