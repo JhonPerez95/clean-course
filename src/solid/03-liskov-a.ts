554faf3275f81d5785db4e2ef0660fa6d2ef78d3
jhonperez95 <jhonperez.j95@gmail.com>
2022-06-22 00:06:18 -0500

    terminando principio de Liskov

--- src/solid/03-liskov-a.ts
+++ src/solid/03-liskov-a.ts
@@ -1,27 +1,18 @@
-import { Tesla, Audi, Toyota, Honda, Volvo } from './03-liskov-b'
+import { Tesla, Audi, Toyota, Honda, Volvo, Vehicle } from './03-liskov-b'
 ;(() => {
-  const printCarSeats = (cars: (Tesla | Audi | Toyota | Honda | Volvo)[]) => {
-    for (const car of cars) {
-      if (car instanceof Tesla) {
-        console.log('Tesla', car.getNumberOfTeslaSeats())
-        continue
-      }
-      if (car instanceof Audi) {
-        console.log('Audi', car.getNumberOfAudiSeats())
-        continue
-      }
-      if (car instanceof Toyota) {
-        console.log('Toyota', car.getNumberOfToyotaSeats())
-        continue
-      }
-      if (car instanceof Honda) {
-        console.log('Honda', car.getNumberOfHondaSeats())
-        continue
-      }
-    }
+  const printCarSeats = (cars: Vehicle[]) => {
+    cars.forEach((car) => {
+      console.log(car.constructor.name, car.getNumberOfSeat())
+    })
   }
 
-  const cars = [new Tesla(7), new Audi(2), new Toyota(5), new Honda(5), new Volvo(2)]
+  const cars = [
+    new Tesla(7),
+    new Audi(2),
+    new Toyota(5),
+    new Honda(5),
+    new Volvo(2),
+  ]
 
   printCarSeats(cars)
 })()
